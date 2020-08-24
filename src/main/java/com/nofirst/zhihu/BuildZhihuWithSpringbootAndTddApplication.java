4d0f1044b51a542b93d0602d4198917775ed24af
meiyiming <1025926969@qq.com>
2020-08-24 17:03:50 +0800

    init

---
+++ src/main/java/com/nofirst/zhihu/BuildZhihuWithSpringbootAndTddApplication.java
@@ -0,0 +1,13 @@
+package com.nofirst.zhihu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildZhihuWithSpringbootAndTddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuildZhihuWithSpringbootAndTddApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nofirst/zhihu/BuildZhihuWithSpringbootAndTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nofirst.zhihu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildZhihuWithSpringbootAndTddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
