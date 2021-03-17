3ef532d722ad2a7e6c669d092d658ffb6be7b00c
qianhuihuiji <1025926969@qq.com>
2021-03-17 16:22:18 +0800

    pass first test

---
+++ src/main/java/com/nofirst/zhihu/service/impl/QuestionServiceImpl.java
@@ -0,0 +1,12 @@
+package com.nofirst.zhihu.service.impl;
+
+import com.nofirst.zhihu.mbg.model.Question;
+import com.nofirst.zhihu.service.QuestionsService;
+
+public class QuestionServiceImpl implements QuestionsService {
+
+    @Override
+    public Question show(Long id) {
+        return null;
+    }
+}
