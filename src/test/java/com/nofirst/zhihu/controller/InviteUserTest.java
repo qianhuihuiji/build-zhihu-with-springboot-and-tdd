34ea1770728f6e3018a07e1042a413ee8296b538
qianhuihuiji <1025926969@qq.com>
2024-11-27 19:15:39 +0800

    push to chapter 9.6

--- src/test/java/com/nofirst/zhihu/controller/InviteUserTest.java
+++ src/test/java/com/nofirst/zhihu/controller/InviteUserTest.java
@@ -4,7 +4,9 @@ import cn.hutool.json.JSONUtil;
 import com.nofirst.zhihu.BuildZhihuWithSpringbootAndTddApplication;
 import com.nofirst.zhihu.common.ResultCode;
 import com.nofirst.zhihu.factory.QuestionFactory;
+import com.nofirst.zhihu.mbg.mapper.NotificationMapper;
 import com.nofirst.zhihu.mbg.mapper.QuestionMapper;
+import com.nofirst.zhihu.mbg.model.NotificationExample;
 import com.nofirst.zhihu.mbg.model.Question;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -20,6 +22,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.testcontainers.containers.MySQLContainer;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -38,6 +41,9 @@ class InviteUserTest {
 
     @Autowired
     private QuestionMapper questionMapper;
+
+    @Autowired
+    private NotificationMapper notificationMapper;
 
     @BeforeAll
     public static void start() {
@@ -77,6 +83,11 @@ class InviteUserTest {
         question.setContent("@Jane please help me");
         questionMapper.insert(question);
 
+        NotificationExample notificationExample = new NotificationExample();
+        NotificationExample.Criteria criteria = notificationExample.createCriteria();
+        criteria.andNotifiableIdEqualTo(1);
+        long beforeCount = notificationMapper.countByExample(notificationExample);
+        assertThat(beforeCount).isEqualTo(0);
         // when
         this.mockMvc.perform(post("/questions/{questionId}/published-questions", question.getId())
                         .contentType(MediaType.APPLICATION_JSON)
@@ -86,6 +97,9 @@ class InviteUserTest {
                 .andExpect(jsonPath("$.code").value(ResultCode.SUCCESS.getCode()));
 
         // then
+        long afterCount = notificationMapper.countByExample(notificationExample);
+        assertThat(afterCount).isEqualTo(1);
+
     }
 
     @Test
@@ -97,6 +111,16 @@ class InviteUserTest {
         question.setContent("@Jane @Foo please help me");
         questionMapper.insert(question);
 
+        NotificationExample notificationExample = new NotificationExample();
+        NotificationExample.Criteria criteria = notificationExample.createCriteria();
+        // 查用户id为1的，也就是 Jane 
+        criteria.andNotifiableIdEqualTo(1);
+        long beforeCountOfJane = notificationMapper.countByExample(notificationExample);
+        assertThat(beforeCountOfJane).isEqualTo(0);
+        // 查用户id为3的，也就是 Foo
+        criteria.andNotifiableIdEqualTo(3);
+        long beforeCountOfFoo = notificationMapper.countByExample(notificationExample);
+        assertThat(beforeCountOfFoo).isEqualTo(0);
         // when
         this.mockMvc.perform(post("/questions/{questionId}/published-questions", question.getId())
                         .contentType(MediaType.APPLICATION_JSON)
@@ -106,6 +130,14 @@ class InviteUserTest {
                 .andExpect(jsonPath("$.code").value(ResultCode.SUCCESS.getCode()));
 
         // then
+        // 查用户id为1的，也就是 Jane
+        criteria.andNotifiableIdEqualTo(1);
+        long afterCountOfJane = notificationMapper.countByExample(notificationExample);
+        assertThat(afterCountOfJane).isEqualTo(1);
+        // 查用户id为3的，也就是 Foo
+        criteria.andNotifiableIdEqualTo(3);
+        long afterCountOfFoo = notificationMapper.countByExample(notificationExample);
+        assertThat(afterCountOfFoo).isEqualTo(1);
     }
 
 }
