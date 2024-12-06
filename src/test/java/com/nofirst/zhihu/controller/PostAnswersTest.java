b73feab1ff1f41aa50ec2569eca2c79ce7ed09d1
qianhuihuiji <1025926969@qq.com>
2024-12-06 11:30:23 +0800

    抽取BaseContainerTest

--- src/test/java/com/nofirst/zhihu/controller/PostAnswersTest.java
+++ src/test/java/com/nofirst/zhihu/controller/PostAnswersTest.java
@@ -2,7 +2,6 @@ package com.nofirst.zhihu.controller;
 
 import cn.hutool.json.JSONUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.nofirst.zhihu.BuildZhihuWithSpringbootAndTddApplication;
 import com.nofirst.zhihu.common.ResultCode;
 import com.nofirst.zhihu.dao.AnswerDao;
 import com.nofirst.zhihu.factory.AnswerFactory;
@@ -12,20 +11,16 @@ import com.nofirst.zhihu.mbg.mapper.QuestionMapper;
 import com.nofirst.zhihu.mbg.model.Answer;
 import com.nofirst.zhihu.mbg.model.Question;
 import com.nofirst.zhihu.model.dto.AnswerDto;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.context.support.WithUserDetails;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-import org.testcontainers.containers.MySQLContainer;
 
 import java.util.List;
 
@@ -36,9 +31,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-// 会启动完整的Spring容器，因此会非常耗时
-@SpringBootTest(classes = BuildZhihuWithSpringbootAndTddApplication.class)
-class PostAnswersTest {
+@Tag("just")
+public class PostAnswersTest extends BaseContainerTest {
 
     private MockMvc mockMvc;
     @Autowired
@@ -56,10 +50,6 @@ class PostAnswersTest {
     @Autowired
     private QuestionMapper questionMapper;
 
-    @BeforeAll
-    public static void start() {
-        mySQLContainer.start();
-    }
 
     @BeforeEach
     public void setup() {
@@ -70,20 +60,6 @@ class PostAnswersTest {
 
         questionMapper.deleteByExample(null);
         answerMapper.deleteByExample(null);
-    }
-
-    // 这里的 mysql:8.0 镜像最好先本地下载，不然启动测试会先尝试下载，测试时间会变得非常长
-    public static MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:8.0")
-            .withDatabaseName("zhihu")
-            .withUsername("root")
-            .withPassword("root")
-            .withReuse(true);
-
-    @DynamicPropertySource
-    static void properties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
-        registry.add("spring.datasource.password", mySQLContainer::getPassword);
-        registry.add("spring.datasource.username", mySQLContainer::getUsername);
     }
 
 
--- src/test/java/com/nofirst/zhihu/controller/QuestionCommentsTest.java
+++ src/test/java/com/nofirst/zhihu/controller/QuestionCommentsTest.java
@@ -1,22 +1,17 @@
 package com.nofirst.zhihu.controller;
 
-import com.nofirst.zhihu.BuildZhihuWithSpringbootAndTddApplication;
 import com.nofirst.zhihu.common.ResultCode;
 import com.nofirst.zhihu.factory.QuestionFactory;
 import com.nofirst.zhihu.mbg.mapper.QuestionMapper;
 import com.nofirst.zhihu.mbg.model.Question;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithUserDetails;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-import org.testcontainers.containers.MySQLContainer;
 
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -24,9 +19,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-// 会启动完整的Spring容器，因此会非常耗时
-@SpringBootTest(classes = BuildZhihuWithSpringbootAndTddApplication.class)
-class QuestionCommentsTest {
+@Tag("just")
+class QuestionCommentsTest extends BaseContainerTest {
 
     private MockMvc mockMvc;
 
@@ -36,10 +30,6 @@ class QuestionCommentsTest {
     @Autowired
     private QuestionMapper questionMapper;
 
-    @BeforeAll
-    public static void start() {
-        mySQLContainer.start();
-    }
 
     @BeforeEach
     public void setup() {
@@ -48,21 +38,6 @@ class QuestionCommentsTest {
                 .apply(springSecurity())
                 .build();
         questionMapper.deleteByExample(null);
-    }
-
-
-    // 这里的 mysql:8.0 镜像最好先本地下载，不然启动测试会先尝试下载，测试时间会变得非常长
-    public static MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:8.0")
-            .withDatabaseName("zhihu")
-            .withUsername("root")
-            .withPassword("root")
-            .withReuse(true);
-
-    @DynamicPropertySource
-    static void properties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
-        registry.add("spring.datasource.password", mySQLContainer::getPassword);
-        registry.add("spring.datasource.username", mySQLContainer::getUsername);
     }
 
 
