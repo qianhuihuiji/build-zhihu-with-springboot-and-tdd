bfe2bc9f27628ef39bc32d6a457ae64bcd317ed9
qianhuihuiji <1025926969@qq.com>
2024-12-10 15:48:19 +0800

    push to chapter 12.4, tests not pass

---
+++ src/test/java/com/nofirst/zhihu/controller/UserProfileTest.java
@@ -0,0 +1,110 @@
+package com.nofirst.zhihu.controller;
+
+import cn.hutool.json.JSONUtil;
+import com.nofirst.zhihu.BaseContainerTest;
+import com.nofirst.zhihu.common.ResultCode;
+import com.nofirst.zhihu.factory.AnswerFactory;
+import com.nofirst.zhihu.factory.QuestionFactory;
+import com.nofirst.zhihu.mbg.mapper.ActivityMapper;
+import com.nofirst.zhihu.mbg.mapper.QuestionMapper;
+import com.nofirst.zhihu.mbg.model.ActivityExample;
+import com.nofirst.zhihu.mbg.model.Question;
+import com.nofirst.zhihu.model.dto.AnswerDto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+class UserProfileTest extends BaseContainerTest {
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    private QuestionMapper questionMapper;
+
+    @Autowired
+    private ActivityMapper activityMapper;
+
+
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
+    @Test
+    @WithUserDetails(value = "John", userDetailsServiceBeanName = "accountUserDetailsService")
+    void a_user_can_see_his_profile_page() throws Exception {
+        // given
+        Question question = QuestionFactory.createUnpublishedQuestion();
+        question.setUserId(2);
+        questionMapper.insert(question);
+        ActivityExample example = new ActivityExample();
+        ActivityExample.Criteria criteria = example.createCriteria();
+        criteria.andSubjectIdEqualTo(question.getId());
+        criteria.andSubjectTypeEqualTo(Question.class.getSimpleName());
+        criteria.andTypeEqualTo("published_question");
+        long beforeCount = activityMapper.countByExample(example);
+        assertThat(beforeCount).isEqualTo(0);
+
+        // when
+        this.mockMvc.perform(get("/profiles/{userId}", 2))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code").value(ResultCode.SUCCESS.getCode()));
+
+        // then
+        long afterCount = activityMapper.countByExample(example);
+        // 调用之后增加了 1 条
+        assertThat(afterCount).isEqualTo(1);
+    }
+
+    @Test
+    @WithUserDetails(value = "John", userDetailsServiceBeanName = "accountUserDetailsService")
+    void it_records_activity_when_an_answer_is_created() throws Exception {
+        // given
+        // 创建一个 question
+        Question question = QuestionFactory.createPublishedQuestion();
+        questionMapper.insert(question);
+
+        ActivityExample example = new ActivityExample();
+        ActivityExample.Criteria criteria = example.createCriteria();
+        criteria.andSubjectIdEqualTo(question.getId());
+        criteria.andSubjectTypeEqualTo(Question.class.getSimpleName());
+        criteria.andTypeEqualTo("created_answer");
+        long beforeCount = activityMapper.countByExample(example);
+        assertThat(beforeCount).isEqualTo(0);
+
+        AnswerDto answer = AnswerFactory.createAnswerDto();
+        this.mockMvc.perform(post("/questions/{id}/answers", question.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(JSONUtil.toJsonStr(answer))
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code").value(ResultCode.SUCCESS.getCode()));
+
+        // then
+        long afterCount = activityMapper.countByExample(example);
+        // 调用之后增加了 1 条
+        assertThat(afterCount).isEqualTo(1);
+    }
+
+}
